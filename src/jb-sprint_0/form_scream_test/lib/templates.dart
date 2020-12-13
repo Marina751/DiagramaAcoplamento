f94422bbeff84e2ac132f926e398db944b198824
breno <joao.breno.ro17@gmail.com>
2020-12-12 22:07:19 -0300

    template

--- src/jb-sprint_0/form_scream_test/lib/templates.dart
+++ src/jb-sprint_0/form_scream_test/lib/templates.dart
@@ -50,37 +50,3 @@ class TemplateFormRegister extends StatelessWidget {
     );
   }
 }
-
-/*
-BottomAppBar(
-          child: Container(
-            height: 50.0,
-            child: Center(
-              child: Text.rich(
-                TextSpan(
-                  text: 'Ao se cadastrar, você concorda com os ',
-                  children: <TextSpan>[
-                    TextSpan(
-                      text: 'Termos de Serviço',
-                      style: TextStyle(
-                        color: Colors.blue,
-                        decoration: TextDecoration.underline,
-                      ),
-                    ),
-                    TextSpan(text: ' e '),
-                    TextSpan(
-                      text: 'Política de Privacidade',
-                      style: TextStyle(
-                        color: Colors.blue,
-                        decoration: TextDecoration.underline,
-                      ),
-                    ),
-                    TextSpan(text: ' do Freckt'),
-                  ],
-                ),
-                textAlign: TextAlign.center,
-              ),
-            ),
-          ),
-        ),
- */
