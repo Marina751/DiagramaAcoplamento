fefd0f60797e84e410482633b8fa106a79b84fb9
Ana Lidia Lisboa de Andrade <analidialisboadeand@gmail.com>
2020-12-21 16:33:21 -0300

    Modificação

--- src/freckt_cliente/lib/main.dart
+++ src/freckt_cliente/lib/main.dart
@@ -4,6 +4,7 @@ import 'package:freckt_cliente/screens/entrar.dart';
 import 'package:freckt_cliente/screens/verif_numero.dart';
 import 'package:freckt_cliente/screens/verif_aprovada.dart';
 import 'package:freckt_cliente/screens/verif_negada.dart';
+import 'package:freckt_cliente/screens/splash_screem.dart';
 
 void main() {
   runApp(
@@ -18,6 +19,7 @@ void main() {
         '/vfap': (context) => VerifAprovada(),
         '/aul': (context) => AutorizacaoLegal(),
         '/ent': (context) => Entrar(),
+        '/splsh': (context) => Splash(),
       },
     ),
   );
@@ -27,44 +29,50 @@ class Root extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Root')),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ElevatedButton(
-              child: Text('VRNU'),
-              onPressed: () {
-                Navigator.pushNamed(context, '/vfnu');
-              },
-            ),
-            ElevatedButton(
-              child: Text('VRNE'),
-              onPressed: () {
-                Navigator.pushNamed(context, '/vfne');
-              },
-            ),
-            ElevatedButton(
-              child: Text('VRAP'),
-              onPressed: () {
-                Navigator.pushNamed(context, '/vfap');
-              },
-            ),
-            ElevatedButton(
-              child: Text('AUL'),
-              onPressed: () {
-                Navigator.pushNamed(context, '/aul');
-              },
-            ),
-            ElevatedButton(
-              child: Text('ENT'),
-              onPressed: () {
-                Navigator.pushNamed(context, '/ent');
-              },
-            ),
-          ],
+        appBar: AppBar(title: Text('Root')),
+        body: Center(
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+          ElevatedButton(
+          child: Text('VRNU'),
+          onPressed: () {
+            Navigator.pushNamed(context, '/vfnu');
+          },
         ),
-      ),
+        ElevatedButton(
+          child: Text('VRNE'),
+          onPressed: () {
+            Navigator.pushNamed(context, '/vfne');
+          },
+        ),
+        ElevatedButton(
+          child: Text('VRAP'),
+          onPressed: () {
+            Navigator.pushNamed(context, '/vfap');
+          },
+        ),
+        ElevatedButton(
+          child: Text('AUL'),
+          onPressed: () {
+            Navigator.pushNamed(context, '/aul');
+          },
+        ),
+        ElevatedButton(
+          child: Text('ENT'),
+          onPressed: () {
+            Navigator.pushNamed(context, '/ent');
+          },
+        ),
+        ElevatedButton(
+          child: Text('SPLASH'),
+          onPressed: () {
+            Navigator.pushNamed(context, '/splsh');
+          },
+        ),
+    ],
+    ),
+    ),
     );
+    }
   }
-}
--- src/freckt_cliente/lib/screens/splash_screem.dart
+++ src/freckt_cliente/lib/screens/splash_screem.dart
@@ -1,17 +1,50 @@
 import 'package:flutter/material.dart';
+import 'entrar.dart';
+import 'dart:async';
+import 'package:google_fonts/google_fonts.dart';
 
-  class Splash extends StatefulWidget {
-    @override
-    _SplashState createState() => _SplashState();
+class Splash extends StatefulWidget {
+  @override
+  _SplashState createState() => _SplashState();
+}
+
+class _SplashState extends State<Splash> {
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    Timer(Duration(seconds: 2), () {
+      Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => Entrar(),
+          ));
+    });
   }
 
-  class _SplashState extends State<Splash> {
-    @override
-    Widget build(BuildContext context) {
-      return Scaffold(
-        body: local_shipping_rounded(size: 400),
-
-        );
-    }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.tealAccent[700],
+      body: SingleChildScrollView(
+        padding: EdgeInsets.fromLTRB(20.0, 330.0, 20.0, 20.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(Icons.local_shipping_rounded, size: 68.0),
+            Padding(
+              padding: EdgeInsets.only(left: 10.0),
+              child: Text(
+                'freckt',
+                style: GoogleFonts.comfortaa(
+                  fontSize: 48.0,
+                  color: Colors.white,
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
-
+}
