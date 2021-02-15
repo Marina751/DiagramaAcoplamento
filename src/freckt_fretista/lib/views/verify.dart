0245447e20692bc9d663ba4d23d9b10b2dd09330
mgradvohl <marinagradvohl@gmail.com>
2021-02-15 15:12:16 -0300

    Configura a tela de verificação do email

--- src/freckt_fretista/lib/views/verify.dart
+++ src/freckt_fretista/lib/views/verify.dart
@@ -2,6 +2,9 @@ import 'dart:async';
 
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:freckt_fretista/utils/templates/elevated_button_template.dart';
+import 'package:freckt_fretista/utils/templates/scaffold_template.dart';
+import 'package:freckt_fretista/utils/templates/submenu_scaffold_template.dart';
 import 'package:freckt_fretista/views/home_fretista.dart';
 
 class VerifyEmailScreen extends StatefulWidget {
@@ -19,21 +22,36 @@ class _VerifyEmailScreenState extends State<VerifyEmailScreen> {
     user = auth.currentUser;
     user.sendEmailVerification();
 
-    Timer.periodic(Duration(seconds: 3), (timer) {});
+    timer = Timer.periodic(Duration(seconds: 3), (timer) {
+      checkEmailVerified();
+    });
     super.initState();
   }
 
   @override
+  void dispose() {
+    timer.cancel();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return SubmenuScaffoldTemplate(
+      title: 'Verificação de Email',
+      body: Column(children: [
+        Text("Um email de verificação foi enviado para ${user.email}. Por favor cheque na sua Caixa de Entrada."),
+        Text('Não recebeu? Reenviar email.'),
+      ]),
+    );
   }
 
   Future<void> checkEmailVerified() async {
     user = auth.currentUser;
     await user.reload();
     if (user.emailVerified) {
+      timer.cancel();
       Navigator.of(context).pushReplacement(
-        MaterialPageRoute(builder: (context) => HomeFretista()));
+          MaterialPageRoute(builder: (context) => HomeFretista()));
     }
   }
 }
