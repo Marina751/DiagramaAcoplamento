454555689ce1e1b3e52c92c79e35d7a2df947450
mgradvohl <marinagradvohl@gmail.com>
2021-02-15 17:44:42 -0300

    Finaliza o design da tela de verificação do email

--- src/freckt_fretista/lib/views/verify_email.dart
+++ src/freckt_fretista/lib/views/verify_email.dart
@@ -2,9 +2,8 @@ import 'dart:async';
 
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:freckt_fretista/utils/consts.dart';
 import 'package:freckt_fretista/utils/templates/elevated_button_template.dart';
-import 'package:freckt_fretista/utils/templates/scaffold_template.dart';
-import 'package:freckt_fretista/utils/templates/submenu_scaffold_template.dart';
 import 'package:freckt_fretista/views/home_fretista.dart';
 
 class VerifyEmailScreen extends StatefulWidget {
@@ -36,13 +35,34 @@ class _VerifyEmailScreenState extends State<VerifyEmailScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return SubmenuScaffoldTemplate(
-      title: 'Verificação de Email',
-      body: Column(children: [
-        Text("Um email de verificação foi enviado para ${user.email}. Por favor cheque na sua Caixa de Entrada."),
-        Text('Não recebeu? Reenviar email.'),
-      ]),
-    );
+    return Column(children: [
+      Expanded(child:
+        Icon(Icons.outgoing_mail, color: Consts.greenDark, size: 80,),
+      ),
+      Expanded(child: 
+        Column(children: [
+          Text(
+            "Um email foi enviado para\n",
+            textAlign: TextAlign.center,
+            style: TextStyle(fontSize: 20),
+          ),
+          Text(
+            '${user.email}\n',
+            textAlign: TextAlign.center,
+            style: TextStyle(fontSize: 23, fontWeight: FontWeight.bold),
+          ),
+          Text(
+            "Clique no link do email recebido para validar sua conta e começar a utilizar o aplicativo",
+            textAlign: TextAlign.center,
+            style: TextStyle(fontSize: 17,),
+          ),
+        ])
+      ),
+      Text('Não recebeu?', style: TextStyle(color: Colors.grey)),
+      ElevatedButtonTemplate(
+        onPressed: () {}, 
+        buttonText: 'REENVIAR EMAIL')
+    ]);
   }
 
   Future<void> checkEmailVerified() async {
